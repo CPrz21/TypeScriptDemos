ae9b77b507936723c8133b9c38d6971f77ad7233
CPrz21 <faselona@gmail.com>
2021-02-23 07:36:17 -0600

    add notes for this section

--- examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
+++ examples/object-oriented/3 - Classes and Objects/src/scripts/checking-account.ts
@@ -1,4 +1,30 @@
-﻿export class CheckingAccount {
+﻿//? NOTES:
+//* CONSTRUCTOR 
+//* is called when a class is instantiated to create an object, 
+//* can accept parameters that can be mapped to properties
+
+//* AUTOMATIC PROPERTIES(AP)
+//* Properties can be defined in a constructor using accesibility modifiers (public/private)
+//* by using it (AP) a property will be generated and the contructor value will be mapped to it
+//! Example
+/*
+Original
+class Person {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+With AP
+class Person {
+    constructor(public firstName: string, public lastName: string){}
+}
+*/
+
+export class CheckingAccount {
     private _balance = 0; // field
 
     constructor(public title: string) { }
