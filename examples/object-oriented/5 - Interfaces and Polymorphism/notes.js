c38cfd7bf236f5fbafd5355cb83b621dfe268bd0
CPrz21 <faselona@gmail.com>
2021-02-25 11:31:56 -0600

    add notes from this section

---
+++ examples/object-oriented/5 - Interfaces and Polymorphism/notes.js
@@ -0,0 +1,24 @@
+//? Interfaces
+//* Act as a contract that defines a set of rules
+
+//* Can be used to create custom data types
+
+//* Classes can implement interfaces wich can help ensure consitency across an application by using the IMPLEMENTS keyword
+
+//* Interfaces are only uses during development(no impact on script/bundle size)
+//! Example
+/**
+ * interface IPerson {
+ *  name: string;
+ *  age: number;
+ *  deposit(amount: number): void;
+ * }
+ */
+
+//* Interfaces can be used as types to define the shape of data held ina property or 
+//* that is passed to a function/method or contructor
+
+ //? Polymorphism
+//* Objects exhibit the same behavior but in a different way
+
+//*
